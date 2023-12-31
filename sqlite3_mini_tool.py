1c630fc9ac699ef6a3f6e8f0c958a8b41024cc67
yugu0202 <applerin1119@gmail.com>
2023-12-31 14:04:05 +0900

    ファイルからのインポート, テーブルの削除を追加

--- sqlite3_mini_tool.py
+++ sqlite3_mini_tool.py
@@ -1,5 +1,6 @@
 from fire import Fire
 import sqlite3
+import json
 
 def create_table(db_name, table_name, *columns):
     columns_with_types = list(map(lambda x: [x[0],"string"] if len(x) == 1 else x,[c.split(":") for c in columns]))
@@ -10,6 +11,47 @@ def create_table(db_name, table_name, *columns):
     c.execute(f"CREATE TABLE {table_name} ({columns_text})")
     conn.commit()
     conn.close()
+
+def insert_into_table(db_name, table_name, *values):
+    values_text = ", ".join([f"'{v}'" for v in values])
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute(f"INSERT INTO {table_name} VALUES ({values_text})")
+    conn.commit()
+    conn.close()
+
+def delete_table(db_name, table_name):
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute(f"DROP TABLE {table_name}")
+    conn.commit()
+    conn.close()
+
+def create_tables_from_file(db_name, file):
+    with open(file) as f:
+        lines = [l.strip() for l in f.readlines()]
+    for line in lines:
+        if line.startswith("CREATE TABLE"):
+            table_name = line.split(" ")[2]
+            columns = line.split("(")[1].split(")")[0].split(",")
+            create_table(db_name, table_name, *columns)
+        elif line.startswith("INSERT INTO"):
+            table_name = line.split(" ")[2]
+            values = line.split("(")[1].split(")")[0].split(",")
+            insert_into_table(db_name, table_name, *values)
+
+def create_from_csv(db_name, table_name, csv_file):
+    with open(csv_file) as f:
+        columns = f.readline().strip().split(",")
+        create_table(db_name, table_name, *columns)
+        map(lambda x: insert_into_table(db_name, table_name, *x.strip().split(",")), f.readlines())
+
+def create_from_json(db_name, table_name, json_file):
+    with open(json_file) as f:
+        columns = list(json.load(f)[0].keys())
+        create_table(db_name, table_name, *columns)
+        map(lambda x: insert_into_table(db_name, table_name, *x.values()), json.load(f))
 
 def view_tables(db_name):
     conn = sqlite3.connect(db_name)
